8dd584c13f97bae3cc3166678551a4ae6320f39d
CahyaAji <cahya.akk@gmail.com>
2023-05-07 17:20:24 +0700

    edit img src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/logo_mtj.svg" />
+        <link rel="icon" type="image/svg+xml" href="logo_mtj.svg" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Menoreh Tanjung Jaya</title>
